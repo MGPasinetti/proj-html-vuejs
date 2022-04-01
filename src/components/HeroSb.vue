1f2c9205f687a4f8ae7b7baa9eac59da17086411
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-01 17:58:57 +0200

    inserting components

---
+++ src/components/HeroSb.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+      <!-- COL-1 -->
+      <div></div>
+
+      <!-- COL-2 -->
+      <div></div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'HeroSb',
+}
+</script>
+
+<style>
+
+</style>
